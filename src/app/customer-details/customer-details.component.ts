6518aa86672ab0054e012520e7a698c0df8f5db9
Alex Eden <alexandereden91@gmail.com>
2018-06-03 03:47:28 -0500

    Add customer details

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,7 +1,9 @@
+import { Id, Customer } from '@crm/lib';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, switchMap } from 'rxjs/operators';
+import { CustomersService } from '../customers.service';
 
 @Component({
   selector: 'crm-customer-details',
@@ -10,15 +12,21 @@ import { map, filter } from 'rxjs/operators';
 })
 export class CustomerDetailsComponent implements OnInit {
   cid: Observable<string>;
+  details: Observable<Id<Customer>>;
+
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private customerService: CustomersService
   ) {
     this.cid = this.route.paramMap.pipe(
       map(params => params.get('cid')),
       filter<string>(cid => cid !== null)
     );
-    //
+
+    this.details = this.cid.pipe(
+      switchMap(cid => this.customerService.customerById(cid))
+    );
   }
 
   ngOnInit() {

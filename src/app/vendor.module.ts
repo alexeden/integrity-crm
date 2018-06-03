8590b528498448646846c8e0cd8426afd0ac5916
Alex Eden <alexandereden91@gmail.com>
2018-06-03 01:45:56 -0500

    Add more material modules

--- src/app/vendor.module.ts
+++ src/app/vendor.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
     BrowserAnimationsModule,
     MatButtonModule,
+    MatExpansionModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,

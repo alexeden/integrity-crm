87fad587f99481c5969298574ad8fd0dfb80ab86
Alex Eden <alexandereden91@gmail.com>
2018-06-05 00:10:23 -0500

    Add validators to the form

--- src/app/customer-details/edit-transaction/edit-transaction.component.ts
+++ src/app/customer-details/edit-transaction/edit-transaction.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Transaction } from '@crm/lib';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'crm-edit-transaction',
   templateUrl: './edit-transaction.component.html',
   styleUrls: ['./edit-transaction.component.scss'],
 })
-export class EditTransactionComponent implements OnInit {
+export class EditTransactionComponent implements OnInit, OnChanges {
   readonly form: FormGroup;
+  @Input() transaction: Partial<Transaction> = {};
 
   constructor(
     private fb: FormBuilder
   ) {
     (window as any).editTransactionComponent = this;
     this.form = this.fb.group({
-      amount: [0],
+      amount: this.fb.control(0, [Validators.required]),
       type: ['payment'],
       timestamp: [new Date()],
     });
+  }
+
+  get valid(): boolean {
+    return this.form.valid;
+  }
+
+  get value(): Transaction {
+    const { value } = this.form;
+    return {
+      ...value,
+      timestamp: value.timestamp instanceof Date ? value.timestamp.getTime() : Date.now(),
+    };
+  }
+
+  ngOnChanges({ transaction: { currentValue } }: SimpleChanges) {
+    if (!!currentValue) {
+      this.form.patchValue({
+        ...currentValue,
+        timestamp: new Date(currentValue.timestamp || Date.now()),
+      });
+    }
   }
 
   ngOnInit() {

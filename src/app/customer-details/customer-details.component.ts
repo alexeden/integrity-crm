2ef55362c3a53cf9b9694747b00d3d20f79e10f4
Alex Eden <alexandereden91@gmail.com>
2018-06-06 00:54:01 -0500

    Persist tab selection via query param

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,30 +1,34 @@
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { map, filter, switchMap, takeUntil } from 'rxjs/operators';
 import { AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map, filter, switchMap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 import { Customer, Transaction } from '@crm/lib';
-import { tag } from '@crm/shared';
 import { CustomersService } from '../customers.service';
+// import { tag } from '@crm/shared';
 
 @Component({
   selector: 'crm-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.scss'],
 })
-export class CustomerDetailsComponent implements OnDestroy {
+export class CustomerDetailsComponent implements OnInit, OnDestroy {
 
   cid: Observable<string>;
   customer: Observable<Customer>;
   customerDocument: Observable<AngularFirestoreDocument<Customer>>;
   transactionCollection: Observable<AngularFirestoreCollection<Transaction>>;
 
+  readonly selectedTabIndex = new BehaviorSubject<number>(0);
   private readonly unsubscribe = new Subject<any>();
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     public customerService: CustomersService
   ) {
+    (window as any).customerDetailsComponent = this;
     this.cid = this.route.paramMap.pipe(
       map(params => params.get('cid')),
       filter<string>(cid => cid !== null)
@@ -40,15 +44,27 @@ export class CustomerDetailsComponent implements OnDestroy {
 
     this.customer = this.customerDocument.pipe(
       switchMap(doc => doc.valueChanges()),
-      tag('customer document value'),
       filter<Customer>(c => !!c)
     );
   }
 
+  ngOnInit() {
+    this.route.queryParamMap.pipe(
+      takeUntil(this.unsubscribe),
+      map(query => +(query.get('tab') || 0))
+    )
+    .subscribe(this.selectedTabIndex);
+  }
+
+
+  tabChanged(event: MatTabChangeEvent) {
+    this.router.navigate([], {
+      queryParams: { tab: event.index },
+    });
+  }
+
   ngOnDestroy() {
-    console.log(`CustomerDetailsComponent is being DESTROYED`);
     this.unsubscribe.next('Close all the streams');
     this.unsubscribe.unsubscribe();
   }
-
 }

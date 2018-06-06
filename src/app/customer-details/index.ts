a0d8b9b1c007c04a96f356fb249d26a5d5df80e0
Alex Eden <alexandereden91@gmail.com>
2018-06-06 16:43:57 -0500

    Rename the edit-transaction component create-transaction

--- src/app/customer-details/index.ts
+++ src/app/customer-details/index.ts
@@ -1,4 +1,4 @@
+export { CreateTransactionComponent } from './create-transaction';
 export { CustomerDetailsComponent } from './customer-details.component';
-export { EditTransactionComponent } from './edit-transaction';
 export { NoCustomerSelectedComponent } from './no-customer-selected.component';
 export { TransactionListComponent } from './transaction-list';

1e7afdcb3dd794ba77143ae1e56beae1d62ebdd0
Alex Eden <alexandereden91@gmail.com>
2018-06-06 19:11:15 -0500

    Clean up the customer list

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,35 +1,38 @@
+import { from } from 'rxjs';
+import { take, switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
-import { Component, OnInit } from '@angular/core';
-import { CustomersService } from '../customers.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CreateCustomerComponent } from '@crm/app/create-customer';
-import { take } from 'rxjs/operators';
+import { CustomersService } from '../customers.service';
 
 @Component({
   selector: 'crm-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss'],
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent {
 
   constructor(
-    public dialog: MatDialog,
+    private router: Router,
+    private dialog: MatDialog,
+    private notifier: MatSnackBar,
     public customerService: CustomersService
-  ) {
-    //
-  }
-
-  ngOnInit() {
-    //
-  }
+  ) {}
 
   createNewCustomer() {
     const dialogRef = this.dialog.open(CreateCustomerComponent);
 
-    dialogRef.componentInstance.newCustomer
-      .pipe(take(1))
-      .subscribe(async customer => {
-        await this.customerService.createCustomer(customer);
-        dialogRef.close();
-      });
+    dialogRef.componentInstance.newCustomer.pipe(
+      take(1),
+      switchMap(c => from(this.customerService.createCustomer(c)))
+    )
+    .subscribe(async ({ name, id }) => {
+      this.notifier.open(`✅ ${name.firstName} ${name.lastName} was added as a customer`);
+      dialogRef.close();
+      // Ideally the magic number tab index of 3 would be stored in a constant somewhere
+      this.router.navigate(['customers', id], {queryParams: {tab: 3}});
+    });
   }
 }

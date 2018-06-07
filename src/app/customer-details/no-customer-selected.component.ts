bbd6c3a88cda950f5550d0129589b969a53bc7a4
Alex Eden <alexandereden91@gmail.com>
2018-06-07 00:53:36 -0500

    Lint fix before deploy

--- src/app/customer-details/no-customer-selected.component.ts
+++ src/app/customer-details/no-customer-selected.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './no-customer-selected.component.html',
-  styleUrls: [ './no-customer-selected.component.scss' ]
+  styleUrls: [ './no-customer-selected.component.scss' ],
 })
 export class NoCustomerSelectedComponent {}
